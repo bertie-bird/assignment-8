d4ef897767e277246233acde3411f710a873537d
bertie-bird <1brielle.bitting@gmail.com>
2026-04-22 20:51:02 -0500

    Finalized the scoring system

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 console.log("script.js connected!");
+
+let userAnswers = [];
+
+let buttons = document.querySelectorAll(".answer-btn");
+
+buttons.forEach(function(button, index){
+  button.addEventListener("click", function() {
+    
+    userAnswers[index] = button.dataset.answer;
+
+    button.classList.add("selected");
+  });
+});
+
+function displayResult(){
+    let score = 0;
+    
+    for (let answer of userAnswers) {
+        if (answer === "A") score += 1;
+        else if (answer === "B") score += 2;
+        else if (answer === "C") score += 3;
+        else if (answer === "D") score +=4;
+    }
+
+    let result = "";
+
+    if (score <= 8){
+        result = "Friends in Low Places";
+    }
+    else if (score <= 12){
+        result = "Man! I Feel Like a Woman";
+    }
+    else if (score <= 16){
+        result = "Chattahoochee";
+    }
+    else {
+        result = "Boot Scootin' Boogie";
+    }
+document.getElementById("result-text").textContent = result;
+document.getElementById("result-container").style.display = "block";
+}
+
+document.getElementById("show-result").addEventListener("click", displayResult);
